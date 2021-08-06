6a31f10a718d99a3b644a0362dbfef21c54a88fe
GitHub Action <action@github.com>
2021-08-06 03:39:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/oneyoungg/plugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/oneyoungg/plugindemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.oneyoungg.plugindemo.services
 
+import com.github.oneyoungg.plugindemo.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
